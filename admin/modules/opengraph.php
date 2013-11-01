041d12e50e74c6c97667472c034921e37a0c918c
catchthemes <catchthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-01 07:06:22 +0000

    #Catch Web Tools Plugin Releasing Version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/catch-web-tools/tags/0.1@797094 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/modules/opengraph.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * @package Admin
+ * @sub-package Admin Opengraph Display
+ */
+ ?>
+<div id="catchwebtools" class="wrap">    
+	<?php include ( 'header.php' ); ?>
+    <?php include ( 'navigation.php' ); ?>
+    <div id="opengraph">
+        <form method="post" action="options.php">
+            <?php settings_fields( 'opengraph-settings-group' ); ?>
+            <?php $settings	=	get_catchwebtools_options( 'catchwebtools_opengraph' ); ?>
+			<div class="option-container">
+                <h3 class="option-toggle option-active"><a href="#"><?php _e( 'Enable Open Graph Module', 'catchwebtools' ); ?></a></h3>
+                <div class="option-content inside open">
+                    <table class="form-table">
+                    <tbody>
+                        <tr>
+                            <th scope="row"><?php _e( 'Enable Open Graph Module', 'catchwebtools' ); ?></th>
+                            <td>
+                            <?php 
+                            $text	=	( ! empty ( $settings['status'] ) && $settings['status'] ) ? 'checked' : '';
+                            echo '<input type="checkbox" ' .$text. ' name="catchwebtools_opengraph[status]" value="1"/>&nbsp;&nbsp;'. __( 'Check to Enable', 'catchwebtools' );
+                            echo '<p class="description">'. __( 'Add Open Graph meta data to your site\'s <code>&lt;head&gt;</code> section. You can specify some of the IDs that are sometimes needed below:', 'catchwebtools' ) . '<p class="description">';
+							echo '<p class="description">'. __( 'Open Graph tags for specific Pages or Posts, can be added via Catch Web Tools Custom Meta Box which shows up in Pages\' and Posts\' add/edit sections once this function is enabled.', 'catchwebtools' ) . '</p>';
+                            ?>
+                            </td>
+                        </tr>
+                     </tbody>
+                </table>
+                    <?php submit_button('Save Changes'); ?>
+                </div>
+                
+                <h3 class="option-toggle"><a href="#"><?php _e( 'Facebook Settings', 'catchwebtools' ); ?></a></h3>
+                <div class="option-content inside">
+                    <table class="form-table">
+                    <tbody>
+                        <tr>
+                            <th scope="row"><?php _e( 'Facebook App ID', 'catchwebtools' ); ?></th>
+                            <td>
+                            <?php 
+                            $text	=	( ! empty ( $settings['fb:app_id'] ) ) ? esc_attr( $settings['fb:app_id'] ) : '';
+                            echo '<input type="text" size="80" name="catchwebtools_opengraph[fb:app_id]" value="' .$text. '" />';
+                            ?>
+                            </td>
+                        </tr>
+                     </tbody>
+                </table>
+                    <?php submit_button('Save Changes'); ?>
+                </div>
+                
+               
+                
+                
+                <h3 class="option-toggle"><a href="#"><?php _e( 'Homepage Settings', 'catchwebtools' ); ?></a></h3>
+                <div class="option-content inside">
+                    <table class="form-table">
+                        <tbody>
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'Title', 'catchwebtools' );?>
+                                </th>
+                                <td>
+                                    <?php
+                                    $text	=	( ! empty ( $settings['og:title'] ) ) ? esc_attr( $settings['og:title'] ) : get_bloginfo( 'name' ) ;
+                                    echo '<input type="text" size="80" name="catchwebtools_opengraph[og:title]" value="' .$text. '" /><p id="description">og:title tag</p>';
+                                    ?>
+                                </td>
+                            </tr>
+                            
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'Type', 'catchwebtools' );?>
+                                </th>
+                                <td>
+                                    <?php
+                                    $text	=	( ! empty ( $settings['og:type'] ) ) ? esc_attr( $settings['og:type'] ) : '';
+                                    $options	=	array('website', 'music.song', 'music.radio_station', 'music.playlist', 'music.album', 'video.movie', 'video.tv_show', 'video.episode', 'video.other', 'article', 'book', 'profile');
+                                    echo '<select name="catchwebtools_opengraph[og:type]" class="catchwebtools_opengraph_type">';
+                                    echo '<option value="">-</option>';
+                                    foreach($options as $option){
+                                        echo '<option value="'. $option .'" ';
+                                        if	( $text == $option )	
+                                            echo 'selected="true"';
+                                        echo '>'. $option .'</option>';
+                                    }
+                                    echo '</select><p id="description">og:type tag</p>';
+                                    ?>
+                                </td>
+                            </tr>
+                            
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'URL', 'catchwebtools' );?>
+                                </th>
+                                <td>
+                                    <?php
+                                   $text	=	( ! empty ( $settings['og:url'] ) ) ? esc_attr( $settings['og:url'] ) : home_url();
+                                    echo '<input type="text" size="80" name="catchwebtools_opengraph[og:url]" value="' .$text. '" /><p id="description">og:url tag</p>';
+                                    ?>
+                                </td>
+                            </tr>
+                            
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'Description', 'catchwebtools' );?>
+                                </th>
+                                <td>
+                                    <?php
+                                    $text	=	( ! empty ( $settings['og:description'] ) ) ? esc_attr( $settings['og:description'] ) : esc_attr( get_bloginfo( 'description' ) );
+                                    echo '<textarea cols="80" rows="7" name="catchwebtools_opengraph[og:description]" >' .$text. '</textarea><p id="description">og:description tag</p>';
+                                    ?>
+                                </td>
+                            </tr>
+                            
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'Sitename', 'catchwebtools' );?>
+                                </th>
+                                <td>
+                                    <?php
+                                    $text	=	( ! empty ( $settings['og:site_name'] ) ) ? esc_attr( $settings['og:site_name'] ) : get_bloginfo( 'name' );
+                                    echo '<input type="text" size="80" name="catchwebtools_opengraph[og:site_name]" value="' .$text. '" /><p id="description">og:site_name tag</p>';
+                                    ?>
+                                </td>
+                            </tr>
+                            
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'Locale', 'catchwebtools' );?>
+                                </th>
+                                <td>
+                                    <?php
+                                    $text	=	( ! empty ( $settings['og:locale'] ) ) ? esc_attr( $settings['og:locale'] ) : '';
+                                    echo '<input type="text" size="80" name="catchwebtools_opengraph[og:locale]" value="' .$text. '" /><p id="description">og:locale tag</p>';
+                                    ?>
+                                </td>
+                            </tr>
+                           
+                        </tbody>
+                    </table>
+                <?php submit_button('Save Changes'); ?>   
+                </div>
+                
+                <h3 class="option-toggle"><a href="#"><?php _e( 'Default Settings', 'catchwebtools' ); ?></a></h3>
+                <div class="option-content inside">
+                    <table class="form-table">
+                        <tbody>
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'Default Image', 'catchwebtools' );?>
+                                </th>
+                                <td>
+                                    <?php
+                                    $text	=	( ! empty ( $settings['og:default_image'] ) ) ? esc_attr( $settings['og:default_image'] ) : '';
+                                    echo '
+                                        <input type="text" class="upload-url" size="80" name="catchwebtools_opengraph[og:default_image]" value="'. esc_attr( $text ) .'" />
+                                        <input id="st_upload_button" class="st_upload_button button" name="catchwebtools_opengraph[og:default_image]" type="button" value="'. esc_attr( 'Upload', 'catchwebtools' ) .'" />';
+                                    echo '<p class="description">'. __( 'This image is used if the post/page being shared does not contain any images.', 'catchwebtools' ) .'</p>';
+                                    ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                <?php submit_button('Save Changes'); ?>   
+                </div>
+                
+                <h3 class="option-toggle"><a href="#"><?php _e( 'Custom Settings (Only for Advanced Users)', 'catchwebtools' ); ?></a></h3>
+                <div class="option-content inside">
+                    <table class="form-table">
+                        <tbody>
+                            <tr>                            
+                                <th scope="row">
+                                    <?php _e( 'Custom Tags', 'catchwebtools' ); ?>
+                                </th>
+                                <td>
+                                    <?php
+                                    $text	=	( ! empty ( $settings['custom'] ) ) ? esc_attr( $settings['custom'] ) : '';
+                                    echo '<textarea cols="80" rows="7"  name="catchwebtools_opengraph[custom]" >' .$text. '</textarea>';
+                                    echo '<p class="description">'. __( 'For any other type of Open Graph tags.', 'catchwebtools' ) . '</p>';
+                                    echo '<p class="description">'. __( 'E.g:', 'catchwebtools' ) . '<code>&lt;meta property="og:audio" content="http://example.com/sound.mp3" /&gt;</code>';
+                                    echo '<p class="description">'. __( 'If you do not know what this is, you should probably leave it empty.', 'catchwebtools' );
+                                    ?>
+                                </td>
+                            </tr>
+                         </tbody>
+                    </table>
+                <?php submit_button('Save Changes'); ?>   
+                </div>
+           
+                
+            </div>
+        </form>
+    </div> <!-- #opengraph -->   
+</div><!-- .wrap -->
---
+++ frontend/inc/opengraph-tools.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package Frontend
+ * @sub-package Opengraph tools
+ */
+
+/**
+ * Get the opengraph setting and format output
+ * @return [string] [opengraph information]
+ */
+function catchwebtools_opengraph_display(){
+	$opengraph_settings	=	get_catchwebtools_options( 'catchwebtools_opengraph' );
+	
+	$output		=	'';
+	if( isset( $opengraph_settings['status'] ) &&  $opengraph_settings['status'] ){		
+		
+		unset( $opengraph_settings['status'] );
+		
+		if (is_home() || is_front_page()) {
+			foreach( $opengraph_settings as $property=>$content )
+				if( $property != 'og:default_image' && $content != '' && $property != 'custom')
+					$output	.= '<meta property="' . $property . '" content="' . $content . '"/>'. PHP_EOL ;	
+				else if( $property == 'custom' && $content != '' )
+					$output	.= $content . PHP_EOL ;	
+		}
+		elseif (is_category() || is_archive()) {
+			$output	.= '<meta property="og:title" content="'.single_term_title("", false).'"/>';
+		}			
+		elseif ((is_single() ||  is_page()) && !is_page('blog')) {
+			//Title
+			$get_catchwebtools_opengraph_title	= get_post_meta(get_the_ID(),'catchwebtools_opengraph_title',true);
+			$final_catchwebtools_opengraph_title	=	( !empty( $get_catchwebtools_opengraph_title ) ) ? $get_catchwebtools_opengraph_title :  the_title('','',false) ;
+			
+			//URL
+			$get_catchwebtools_opengraph_url 		= get_post_meta(get_the_ID(),'catchwebtools_opengraph_url',true);
+			$final_catchwebtools_opengraph_url	=	( !empty( $get_catchwebtools_opengraph_url ) ) ? $get_catchwebtools_opengraph_url :  get_permalink() ;
+			
+			//Image
+			$get_catchwebtools_opengraph_image = get_post_meta(get_the_ID(),'catchwebtools_opengraph_image',true);
+			if( empty( $get_catchwebtools_opengraph_image ) &&  $get_catchwebtools_opengraph_image == '' ){
+				$settings 	=	(array) get_option( 'catchwebtools_opengraph' );
+				$image		=	isset( $settings['og:default_image'] ) ? $settings['og:default_image'] : ''; 
+				$get_catchwebtools_opengraph_image = $image;
+			}
+			$final_catchwebtools_opengraph_image	=	( !empty( $get_catchwebtools_opengraph_image ) ) ? $get_catchwebtools_opengraph_image :  '' ;
+			
+			//Description
+			$get_catchwebtools_opengraph_description = get_post_meta(get_the_ID(),'catchwebtools_opengraph_description',true);
+			$final_catchwebtools_opengraph_description	=	( !empty( $get_catchwebtools_opengraph_description ) ) ? $get_catchwebtools_opengraph_description :  '' ;
+			
+			//Type
+			$get_catchwebtools_opengraph_type = get_post_meta(get_the_ID(),'catchwebtools_opengraph_type',true);
+			$final_catchwebtools_opengraph_type	=	( !empty( $get_catchwebtools_opengraph_type ) ) ? $get_catchwebtools_opengraph_type :  '' ;
+			
+			//Custom
+			$get_catchwebtools_opengraph_custom = get_post_meta(get_the_ID(),'catchwebtools_opengraph_custom',true);
+			$final_catchwebtools_opengraph_custom	=	( !empty( $get_catchwebtools_opengraph_custom ) ) ? $get_catchwebtools_opengraph_custom :  '' ;
+			
+			$output	.= '
+				<meta property="og:title" content="'.$final_catchwebtools_opengraph_title.'"/>
+				<meta property="og:url" content="'.$final_catchwebtools_opengraph_url.'"/>
+				<meta property="og:image" content="'.$final_catchwebtools_opengraph_image.'" />
+				<meta property="og:description" content="'.$final_catchwebtools_opengraph_description.'" />
+				<meta property="og:type" content="'.$final_catchwebtools_opengraph_type.'" />
+				';
+			
+			$output	.= $final_catchwebtools_opengraph_custom;
+			
+			
+		}
+	
+	}
+	return $output;
+}
+
+/**
+ * Get Open Graph Html Content
+ * @return [string] [html attribute for open graph]
+ */
+function catchwebtools_add_opengraph_namespace() {
+	return ' prefix="og: http://ogp.me/ns#"' ;
+}
+
+/**
+ * add_filter only if opengraph in enabled
+ */
+$opengraph_settings	=	get_option( 'catchwebtools_opengraph' );
+
+if( isset( $opengraph_settings['status'] ) &&  $opengraph_settings['status'] ){	
+	add_filter( 'language_attributes', 'catchwebtools_add_opengraph_namespace' );
+}
