dd4bde092998dd2dccb17f87c291e2d9e92ec0f3
catchthemes <catchthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-27 19:25:28 +0000

    Version update for compatible with WordPress 3.9.2
    git-svn-id: https://plugins.svn.wordpress.org/catch-web-tools/tags/0.4@974191 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin-functions.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package Admin
+ */
+
+/** 
+ * Enqueue admin scripts and styles
+ */
+function catchwebtools_admin_enqueue_scripts( $hook_suffix ) {
+	//Scripts
+	wp_enqueue_script( 'catchwebtools-plugin-options', CATCHWEBTOOLS_URL . 'admin/js/admin.js', array( 'jquery', 'farbtastic' ), '2013-10-05' );
+		
+	wp_enqueue_script( 'catchwebtools-upload', CATCHWEBTOOLS_URL . 'admin/js/add_image_scripts.js', array( 'jquery','media-upload','thickbox' ) );
+	
+	wp_enqueue_script( 'catchwebtools-equal-height', CATCHWEBTOOLS_URL . 'admin/js/equal_height.js', array( 'jquery' ) );
+	
+	//CSS Styles
+	wp_enqueue_style( 'catchwebtools-genericons', CATCHWEBTOOLS_URL . 'css/genericons.css', false );
+	wp_enqueue_style( 'catchwebtools-plugin-css', CATCHWEBTOOLS_URL . 'admin/css/admin.css', array( 'farbtastic', 'thickbox' ), '2013-10-05' );
+}
+
+/**
+ * Only enqueue scripts on catchwebtools pages
+ */
+if ( isset($_GET['page'] ) && ( $_GET['page'] == 'catch-web-tools' 
+								|| $_GET['page'] == 'catch-web-tools-webmasters'  
+								|| $_GET['page'] == 'catch-web-tools-opengraph' 
+								|| $_GET['page'] == 'catch-web-tools-custom-css' 
+								|| $_GET['page'] == 'catch-web-tools-seo' 
+								|| $_GET['page'] == 'catch-web-tools-social-icons' 
+								|| $_GET['page'] == 'catch-web-tools-catchids' 
+								) )  {
+	add_action( 'admin_enqueue_scripts', 'catchwebtools_admin_enqueue_scripts' );
+}
+
+ /** 
+ * Admin Social Links
+ * use facebook and twitter scripts only on dashboard, hence condition before add_action hook
+ */
+function catchwebtools_admin_social() { ?>
+    <!-- Start Social scripts -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=276203972392824";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    <!-- End Social scripts -->
+<?php
+}
+if ( isset($_GET['page'] ) && ( $_GET['page'] == 'catch-web-tools' ) )  {
+	add_action( 'admin_enqueue_scripts', 'catchwebtools_admin_social' );
+}
+
+
+require_once 'inc/core.php';
+
+require_once 'inc/metabox.php';
+
+require_once 'inc/catch-ids.php';
+
+require_once 'inc/social-icons.php';
---
+++ uninstall.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package Catch Themes
+ * @subpackage Catch Web Tools
+ * @author CatchThemes
+ * @since Catch Web Tools 0.1
+ * Code used when the plugin is removed (not just deactivated but actively deleted through the WordPress Admin).
+ */
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
+    exit();
+
+$options	=	array( 	'catchwebtools_webmaster', 
+						'catchwebtools_opengraph', 
+						'catchwebtools_custom_css', 
+						'catchwebtools_seo', 
+						'catchwebtools_social', 
+						'catchwebtools_catchids' 
+				);
+
+$transient_options	=	array( 	'catchwebtools_webmaster_transient', 
+						'catchwebtools_opengraph_transient', 
+						'catchwebtools_custom_css_transient', 
+						'catchwebtools_seo_transient', 
+						'catchwebtools_social_transient', 
+						'catchwebtools_catchids_transient' 
+				);
+				
+// For Single site
+if ( !is_multisite() ) 
+{
+    foreach ( $options as $option) {
+		delete_option( $option );
+	}
+	foreach ( $transient_options as $option) {
+		delete_transient( $option );
+	}
+} 
+// For Multisite
+else 
+{
+    global $wpdb;
+    $blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+    $original_blog_id = get_current_blog_id();
+    foreach ( $blog_ids as $blog_id ) 
+    {
+		switch_to_blog( $blog_id );
+		foreach ( $options as $option) {
+			delete_site_option( $option );
+		}
+		foreach ( $transient_options as $option) {
+			delete_transient( $option );
+		}
+    }
+    switch_to_blog( $original_blog_id );
+}
