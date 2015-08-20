15e67a3a9f4549d260382b31e2925daced30680f
catchthemes <catchthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-20 14:48:14 +0000

    Catch Web Tools compatible with WordPress version 4.3
    git-svn-id: https://plugins.svn.wordpress.org/catch-web-tools/tags/1.3@1226238 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ frontend/inc/webmasters-tools.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package Frontend
+ * @sub-package Webmaster Tools
+ */
+
+/**
+ * Get the webmaster setting for header and format output
+ * @return [string] [webmaster header information]
+ */
+function catchwebtools_webmaster_header_display(){
+	$webmaster_settings	=	get_catchwebtools_options( 'catchwebtools_webmaster' );
+	
+	$output		=	'';
+	if( isset( $webmaster_settings['status'] ) && $webmaster_settings['status'] ){
+		
+		if( isset( $webmaster_settings['header'] ) && $webmaster_settings['header'] != '' ) 
+			$output .= $webmaster_settings['header'] . PHP_EOL ;
+		
+		if( isset( $webmaster_settings['google-site-verification'] ) && $webmaster_settings['google-site-verification'] != '' ) 
+			$output .= '<meta name="google-site-verification" content="'. esc_attr( $webmaster_settings['google-site-verification'] ) .'" />' . PHP_EOL ;
+		
+		if( isset( $webmaster_settings['msvalidate.01'] ) && $webmaster_settings['msvalidate.01'] != '' ) 
+			$output .= '<meta name="msvalidate.01" content="'. esc_attr( $webmaster_settings['msvalidate.01'] ) .'" />' . PHP_EOL ;
+		
+		if( isset( $webmaster_settings['alexaVerifyID'] ) && $webmaster_settings['alexaVerifyID'] != '' ) 
+			$output .= '<meta name="alexaVerifyID" content="'. esc_attr( $webmaster_settings['alexaVerifyID'] ) .'" />' . PHP_EOL ;
+		
+	}	
+	return $output;
+}
+
+/**
+ * Get the webmaster setting for footer and format output
+ * @return [string] [webmaster footer information]
+ */
+function catchwebtools_webmaster_footer_display(){
+	$webmaster_settings	=	get_option( 'catchwebtools_webmaster' );
+	$output		=	'';
+	if( isset( $webmaster_settings['status'] ) && $webmaster_settings['status'] ){
+		if( isset( $webmaster_settings['footer'] ) && $webmaster_settings['footer'] != '' )
+			$output .= $webmaster_settings['footer'] . PHP_EOL ;
+	}
+	return $output;
+}
+	
